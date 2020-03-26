8e97e05b59fb30a9f5f668e1d66604d0b7352b75
Georgi Slavov <gslavov.nl@gmail.com>
2020-03-26 19:51:00 +0100

    MinimumMeetingRooms by using merge intervals pattern

---
+++ src/test/java/sequences/mergeIntervals/MinimumMeetingRoomsTest.java
@@ -0,0 +1,30 @@
+package sequences.mergeIntervals;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+class MinimumMeetingRoomsTest {
+
+    private MinimumMeetingRooms instance = new MinimumMeetingRooms();
+
+    @Test
+    void testOneOverlap() {
+        var input = new int[][]{{1, 4}, {2, 5}, {7, 9}};
+        assertThat(instance.minMeetingRooms(input), equalTo(2));
+    }
+
+    @Test
+    void testNoOverlap() {
+        var input = new int[][]{{6, 7}, {2, 4}, {8, 12}};
+        assertThat(instance.minMeetingRooms(input), equalTo(1));
+    }
+
+    @Test
+    void testFewOverlaps() {
+        var input = new int[][]{{1, 4}, {2, 3}, {3, 6}};
+        assertThat(instance.minMeetingRooms(input), equalTo(2));
+    }
+
+}
