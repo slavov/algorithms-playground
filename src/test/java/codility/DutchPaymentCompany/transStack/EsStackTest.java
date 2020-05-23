e7b0e2b5cff1d20557592d7a6fe67a2a39bfb8c9
Georgi Slavov <gslavov.nl@gmail.com>
2020-05-23 15:34:15 +0200

    Fix Tests

--- src/test/java/codility/DutchPaymentCompany/transStack/EsStackTest.java
+++ src/test/java/codility/DutchPaymentCompany/transStack/EsStackTest.java
@@ -1,11 +1,10 @@
 package codility.DutchPaymentCompany.transStack;
 
-import codility.DutchPaymentCompany.transStack.EsStack;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 class EsStackTest {
 
