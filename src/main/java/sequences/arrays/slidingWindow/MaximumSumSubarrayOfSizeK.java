66295bd195d223c77e7e5d1a5d8c1c14e1b0282e
Georgi Slavov <gslavov.nl@gmail.com>
2021-10-04 19:07:46 +0200

    MaximumSumSubarrayOfSizeK

--- src/main/java/sequences/arrays/slidingWindow/MaximumSumSubarrayOfSizeK.java
+++ src/main/java/sequences/arrays/slidingWindow/MaximumSumSubarrayOfSizeK.java
@@ -9,18 +9,21 @@ package sequences.arrays.slidingWindow;
  */
 class MaximumSumSubarrayOfSizeK {
 
-    int findMaxSumSubArray(int k, int[] arr) {
-        int windowSum = 0;
-        int windowStart = 0;
+    private static int findMaxSumSubArray(int k, int[] arr) {
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            windowSum += arr[i];
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++) {
+            sum += arr[i];
             if (i >= k - 1) {
-                sum = Math.max(sum, windowSum);
-                windowSum -= arr[windowStart];
-                windowStart++;
+                max = Math.max(max, sum);
+                sum -= arr[i - (k - 1)];
             }
         }
-        return sum;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        var result = MaximumSumSubarrayOfSizeK.findMaxSumSubArray(3, new int[]{2, 1, 5, 1, 3, 2});
+        System.out.println("Maximum Sum Subarray of Size K  " + result);
     }
 }
