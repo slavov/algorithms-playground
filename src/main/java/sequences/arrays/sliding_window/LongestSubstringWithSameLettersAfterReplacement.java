c2d5e2dc8e88eb56f636ae1cb17eef47347effc8
Georgi Slavov <gslavov.nl@gmail.com>
2021-12-13 20:49:15 +0100

    424. Longest Repeating Character Replacement

--- src/main/java/sequences/arrays/sliding_window/LongestSubstringWithSameLettersAfterReplacement.java
+++ src/main/java/sequences/arrays/sliding_window/LongestSubstringWithSameLettersAfterReplacement.java
@@ -7,11 +7,16 @@ import java.util.Map;
  * Given a string with lowercase letters only, if you are allowed to replace no more than ‘k’
  * letters with any letter, find the length of the longest substring having the same letters after
  * replacement.
+ *
+ * <p><a href="https://leetcode.com/problems/longest-repeating-character-replacement/">424. Longest
+ * Repeating Character Replacement</a>
  */
 class LongestSubstringWithSameLettersAfterReplacement {
 
-    int findLength(String str, int k) {
-        int windowStart = 0, max = 0, maxRepeatLetterCount = 0;
+    public int findLength(String str, int k) {
+        int windowStart = 0;
+        int max = 0;
+        int maxRepeatLetterCount = 0;
         Map<Character, Integer> letterFrequencyMap = new HashMap<>();
 
         for (int i = 0; i < str.length(); i++) {
@@ -26,6 +31,29 @@ class LongestSubstringWithSameLettersAfterReplacement {
             }
             max = Math.max(max, i - windowStart + 1);
         }
+        return max;
+    }
+
+    public int characterReplacement(String s, int k) {
+        var l = 0;
+        var max = 0;
+        var maxCount = 0; // max of specific letter count
+        var charCounts = new int[26]; // keep track how many times letter appears.
+
+        for (int r = 0; r < s.length(); r++) {
+            charCounts[s.charAt(r) - 'a']++;
+            maxCount = Math.max(maxCount, charCounts[s.charAt(r) - 'a']);
+
+            // r - l - maxCount gives you how many digestive characters.
+            // E.g  if (r - l - maxCount + 1 > k) that means that we used all allowed transformation
+            while (r - l - maxCount + 1 > k) {
+                charCounts[s.charAt(l) - 'a']--;
+                l++;
+            }
+
+            max = Math.max(max, r - l + 1);
+        }
+
         return max;
     }
 }
