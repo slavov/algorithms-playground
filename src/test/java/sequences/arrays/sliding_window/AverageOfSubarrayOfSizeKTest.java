3f3b34de9e5b8c103f0e98b053a507b81ef33d83
Georgi Slavov <gslavov.nl@gmail.com>
2022-01-18 18:32:36 +0100

    Add Unit Test for AverageOfSubarrayOfSizeK.java

---
+++ src/test/java/sequences/arrays/sliding_window/AverageOfSubarrayOfSizeKTest.java
@@ -0,0 +1,25 @@
+package sequences.arrays.sliding_window;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class AverageOfSubarrayOfSizeKTest {
+
+    private final AverageOfSubarrayOfSizeK instance = new AverageOfSubarrayOfSizeK();
+
+    @ParameterizedTest(name = "Average of subarray of size {1}")
+    @MethodSource
+    void example(int k, int[] input) {
+        var result = instance.findAverages(k, input);
+        assertArrayEquals(new double[] {2.2, 2.8, 2.4, 3.6, 2.8}, result);
+    }
+
+    private static Stream<Arguments> example() {
+        return Stream.of(Arguments.of(5, new int[] {1, 3, 2, 6, -1, 4, 1, 8, 2}));
+    }
+}
